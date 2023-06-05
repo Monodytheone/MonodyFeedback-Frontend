d81af256ba4105b28b307d2cf645af1db9f79600
Monodytheone <monody_tfr@qq.com>
2023-06-06 06:21:09 +0800

    完成FAQ的所有增删改查

--- monodyfeedback-vue3-ts/src/masterViews/pageManage/PageManage.vue
+++ monodyfeedback-vue3-ts/src/masterViews/pageManage/PageManage.vue
@@ -25,6 +25,7 @@
                                 :auto-size="{ minRows: 2, maxRows: 5 }" />
                             <div class="buttons">
                                 <a-button size="small" type="link" @click="handleChangeQandA">修改Q&A</a-button>
+                                <a-button size="small" type="link" @click="handleDeleteQandA" danger>删除Q&A</a-button>
                                 <span style="float:right; ">
                                     <a-button size="small" v-if="index !== 0"><van-icon name="arrow-up"
                                             @click="handleQandAMove(qandA.qandAId, true)" /></a-button>
@@ -42,11 +43,34 @@
                     </a-popover>
                 </div>
                 <div style="clear: both" />
+                <div v-if="data.isPureQandA">
+                    <p style="color: #e67e22; font-weight: bolder; text-align: left; margin-bottom: 0;">Q: </p>
+                    <a-textarea v-model:value="newQuestion" placeholder="Question"
+                        :auto-size="{ minRows: 2, maxRows: 5 }" />
+                    <div style="margin: 15px 0" />
+                    <p style="font-weight: bolder; text-align: left; margin-bottom: 0;">A: </p>
+                    <a-textarea v-model:value="newAnswer" placeholder="Answer" :auto-size="{ minRows: 2, maxRows: 5 }" />
+                    <a-button type="primary" style="margin-top: 10px" @click="handleCreateQandA">新增Q&A</a-button>
+                </div>
+                <div v-if="data.isPureQandA == false">
 
-                <iframe v-if="data.isPureQandA == false" :src="data.htmlUrl" seamless />
-                <!-- <iframe v-if="data.isPureQandA == false" src="static/temp3.html" /> -->
+                    <a-popover title="更换内容源" trigger="hover">
+                        <template #content>
+                            <a-input v-model:value="newHtmlUrl" style="width: 60%" placeholder="输入新的HtmlUrl" />
+                            <a-button style="margin-left: 10px;" @click="handleUpdateHtmlUrl">更新</a-button>
+                        </template>
+                        <span style="cursor: pointer;">
+                            <p style="text-align: left; word-wrap: break-word;"><b>外部Html： </b> {{ data.htmlUrl }}</p>
+                        </span>
+                    </a-popover>
 
-                <div style="height: 500px"></div>
+
+                    <hr>
+                    <iframe :src="data.htmlUrl" seamless />
+                    <!-- <iframe v-if="data.isPureQandA == false" src="static/temp3.html" /> -->
+
+                    <div style="height: 500px"></div>
+                </div>
             </div>
         </div>
     </div>
@@ -59,6 +83,9 @@ import modifyQandA from '@/api/faqManageAPIs/modifyQandA';
 import { message } from 'ant-design-vue';
 import showErrorModal from '@/common/showErrorModal';
 import sortQandAsInPage from '@/api/faqManageAPIs/sortQandAsInPage';
+import deleteQandA from '@/api/faqManageAPIs/deleteQandA';
+import createQandA from '@/api/faqManageAPIs/createQandA';
+import changePageHtmlUrl from '@/api/faqManageAPIs/changePageHtmlUrl';
 export default defineComponent({
     name: 'FAQPage',
     props: {
@@ -72,6 +99,9 @@ export default defineComponent({
         const changedQuestion = ref('')
         const changedAnswer = ref('')
         const showingIndex = ref<number>()
+        const newQuestion = ref('')
+        const newAnswer = ref('')
+        const newHtmlUrl = ref('')
 
         const refreshData = () => {
             masterGetPage(props.pageId)
@@ -118,12 +148,12 @@ export default defineComponent({
                 })
         }
 
-        const handleQandAMove = (qandAId:string, moveForword: Boolean) => {
+        const handleQandAMove = (qandAId: string, moveForword: Boolean) => {
             let qandAIds: string[] = [];
             data.value.qandAs.forEach((qandA: any) => {
                 qandAIds.push(qandA.qandAId);
             })
-            if(moveForword) {
+            if (moveForword) {
                 let temp = qandAIds[showingIndex.value as number];
                 qandAIds[showingIndex.value as number] = qandAIds[showingIndex.value as number - 1];
                 qandAIds[showingIndex.value as number - 1] = temp;
@@ -144,18 +174,62 @@ export default defineComponent({
                 })
         }
 
+        const handleDeleteQandA = () => {
+            deleteQandA(props.pageId, data.value.qandAs[showingIndex.value as number].qandAId)
+                .then(response => {
+                    message.success("删除Q&A成功");
+                    refreshData();
+                })
+                .catch(error => {
+                    showErrorModal(`删除Q&A失败，${error.response.status}: ${error.response.data}`);
+                })
+        }
 
+        const handleCreateQandA = () => {
+            createQandA(props.pageId, newQuestion.value, newAnswer.value)
+                .then(response => {
+                    message.success("新增Q&A成功");
+                    refreshData();
+                    newQuestion.value = '';
+                    newAnswer.value = '';
+                })
+                .catch(error => {
+                    showErrorModal(`新增Q&A失败，${error.response.status}: ${error.response.data}`);
+                })
+        }
+
+        const handleUpdateHtmlUrl = () => {
+            if (newHtmlUrl.value.length === 0) {
+                showErrorModal("请输入新的HtmlUrl后再点击更新");
+                return;
+            }
+            changePageHtmlUrl(props.pageId, newHtmlUrl.value)
+                .then(response => {
+                    message.success("更新内容源成功");
+                    refreshData();
+                    newHtmlUrl.value = '';
+                })
+                .catch(error => {
+                    showErrorModal(`更换内容源失败，${error.response.status}: ${error.response.data}`);
+                })
+        }
 
         return {
             data,
             changedQuestion,
             changedAnswer,
             showingIndex,
+            newQuestion,
+            newAnswer,
+            newHtmlUrl,
             handleBack,
             handlePopoverVisibleChange,
             handleClickQandA,
             handleChangeQandA,
-            handleQandAMove
+            handleQandAMove,
+            handleDeleteQandA,
+            handleCreateQandA,
+            handleUpdateHtmlUrl
         }
     }
 })
